77c57977c67db661df25513dc809b467abb29da6
Soyeon-Cha <7103sy@naver.com>
2024-01-12 17:15:57 +0900

    feat: 초기 세팅

---
+++ src/main/java/CookSave/CookSaveback/CookSaveBackApplication.java
@@ -0,0 +1,13 @@
+package CookSave.CookSaveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookSaveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookSaveBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CookSave/CookSaveback/CookSaveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package CookSave.CookSaveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookSaveBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
